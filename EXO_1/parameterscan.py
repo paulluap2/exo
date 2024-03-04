f18df800702b04d9978217d42a957e231fa75f3a
adriangomezortiz <adrian.gomezortiz@epfl.ch>
2024-03-04 14:57:37 +0100

    Worksession monday
    Graphes

--- EXO_1/parameterscan.py
+++ EXO_1/parameterscan.py
@@ -24,7 +24,7 @@ omega = 10
 mu = 6
 R = 0.033
 rho = 1.2
-v = 0
+v = 5
 g = 9.81
 
 
@@ -35,7 +35,7 @@ a = mu*(R**3)*rho*2*np.pi*omega/m
 
 #Cas avec g==0
 
-"""
+
 x_th = (v/a)*np.sin(a*tfin)
 y_th = (v/a)*(1-np.cos(a*tfin))
 vx_th = v*np.cos(a*tfin)
@@ -51,7 +51,7 @@ y_th  = (g/a**2)*(np.cos(a*tfin)-1)
 vx_th = (g/a)*(1-np.cos(a*tfin))
 vy_th = -(g/a)*np.sin(a*tfin)
 L_th = 2*np.pi*g/a**2
-
+"""
 """
 ... and other parameters
 """
@@ -73,6 +73,10 @@ for i in range(nsimul):
 
 error = np.zeros(nsimul)
 error_energy = np.zeros(nsimul)
+fig, ax = plt.subplots(constrained_layout=True)
+
+lw = 1.5 # line width. TODO: adjust if needed
+fs = 16  # font size. TODO: adjust if needed
 
 for i in range(nsimul):  # Iterate through the results of all simulations
     data = np.loadtxt(outputs[i])  # Load the output file of the i-th simulation
@@ -85,19 +89,26 @@ for i in range(nsimul):  # Iterate through the results of all simulations
     En = data[-1, 5]
     vy_list.append(vy)
     # TODO compute the error for each simulation
-    error[i] = np.sqrt((xx-x_th)**2+(yy-y_th)**2) #pas sûr de ce qu'il faut faire ici
+    error[i] = np.sqrt((xx-x_th)**2+(yy-y_th)**2) 
     error_energy[i] = max(data[:,5])-min(data[:,5])
 
+    ax.plot(t, data[:,5], label=f'nsteps={nsteps[i]}')
 
-lw = 1.5 # line width. TODO: adjust if needed
-fs = 16  # font size. TODO: adjust if needed
-   
+ax.set_xlabel('t [s]', fontsize=fs)
+ax.set_ylabel('E [J]', fontsize=fs)
+ax.legend()
+plt.show()
+
+
+
+
 fig, ax = plt.subplots(constrained_layout=True)
 ax.plot(data[:, 1], data[:, 2])
 ax.set_xlabel('x [m]', fontsize=fs)
 ax.set_ylabel('y [m]', fontsize=fs)
 ax.scatter(x_th, y_th, color='red', label='Exact final position')
-ax.scatter(L_th, 0.0, color='purple', label='L')
+ax.axis('equal')
+#ax.scatter(L_th, 0.0, color='purple', label='L')
 ax.legend()
 
 
